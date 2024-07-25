5956d3704ff7a1e2dcfaa9e4a8183a2282fc02d2
Hassam Azam <hassam.azam@A006-00443.local>
2024-07-25 16:02:43 +0500

    Assignment-3-July-25

--- assignment-3-July-25/script.js
+++ assignment-3-July-25/script.js
@@ -1,43 +1,90 @@
-const stopButtton = () =>{
+const decrementTime = (obj) => {
+    obj.myInterval = setInterval(() => {
+        if (obj.decrementFlag) {
+            obj.seconds -= 1;
+            if (!obj.seconds  ) {
+                if (!obj.minutes) {
+                    stopTimer(obj);
+                } else {
+                    obj.minutes -= 1;
+                    obj.seconds = 59;
+                }
+            } else if (obj.seconds < 0) {
+               
+                stopTimer(obj);
+            }
+            
+            showCurrentTimer(obj);
+        }
+    }, 1000);
+};
 
-}
+const showCurrentTimer = (obj) => {
+   const {minutes,seconds}=obj;
+    const display = obj.div.querySelector('.timerDisplay');
+  
+    display.innerHTML =`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        
+};
 
-const startButton= () =>
-{
-
-}
-
- const timeObj={
-    decrementFlag:false,
-    minutes : 0,
-    seconds : 10,
-    myInterval : null
-
-}
-
-const decrementTime=()=>
-{
+const startTimer = (obj) => {
     
-
-}
-
-const createDocumentElements=()=>
-    {      //creating a new div,with two child buttons
-        const newDiv = document.createElement('div'); 
-        const startButton = document.createElement('button');
-        startButton.innerHTML = 'Start';
-        const stopButton = document.createElement('button');
-        stopButton.innerHTML = 'Stop';
-        //adding events to those buttons
-        startButton.addEventListener('click',startButton);
-        stopButton.addEventListener('click',stopButton);
-        //making the buttons childs of the parent div
-        newDiv.appendChild(startButton);
-        newDiv.appendChild(stopButton);
-        document.body.appendChild(newDiv);
-    }
+    if (obj.minutes === 0 && obj.seconds <= 0) return ;
+    
+    if (obj.timerStarted) clearInterval(obj.myInterval); 
+        
+    obj.decrementFlag = true;
+    decrementTime(obj);
+    obj.timerStarted = true; 
+};
 
 
+const stopTimer = obj => {
+    obj.decrementFlag = false;
+    clearInterval(obj.myInterval);
+    obj.timerStarted = false;
+};
 
-const timeCreate=document.getElementById('TimeCreate');
-timeCreate.addEventListener('click',createDocumentElements);
+const createTimer = () => {
+    const newObj = {
+        decrementFlag: false,
+        timerStarted: false,
+        minutes: 0,
+        seconds: 10,
+        myInterval: null
+    };
+
+    const newDiv = document.createElement('div');
+    const timerDisplay = document.createElement('div');
+    const startButton = document.createElement('button');
+    const stopButton = document.createElement('button');
+
+    newDiv.className="timerDiv";
+
+    timerDisplay.className = 'timerDisplay';  
+    
+    startButton.innerHTML = 'Start';
+    startButton.className = 'startButton'; 
+    startButton.addEventListener('click', () => startTimer(newObj));
+
+    stopButton.innerHTML = 'Stop';
+    stopButton.className = 'stopButton'; 
+    stopButton.addEventListener('click', () => stopTimer(newObj));
+
+    newDiv.appendChild(startButton);
+    newDiv.appendChild(stopButton);
+    newDiv.appendChild(timerDisplay);
+
+    document.body.appendChild(newDiv);
+    document.body.appendChild(document.createElement('br'));
+    
+    startTimer(newObj);
+    
+    newObj.div = newDiv;
+    
+    showCurrentTimer(newObj);
+    
+};
+
+document.getElementById('createButton').addEventListener('click', createTimer);
+
